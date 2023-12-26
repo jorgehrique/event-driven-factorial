30d5046c99d846da2be4962e6b97abfc55c714b9
Jorge Henrique <jorgehasd@gmail.com>
2023-12-26 11:45:06 -0300

    feat: add rabbitmq and producer

--- factorial-service/src/main/java/com/calcltda/factorialservice/factorial/FactorialDTO.java
+++ factorial-service/src/main/java/com/calcltda/factorialservice/factorial/FactorialDTO.java
@@ -1,9 +1,11 @@
 package com.calcltda.factorialservice.factorial;
 
+import java.io.Serializable;
+
 public record FactorialDTO(
         long id,
         long number
-) {
+) implements Serializable {
     public static FactorialDTO fromFactorial(Factorial factorial){
         return new FactorialDTO(
                 factorial.id(),
--- factorial-service/src/main/java/com/calcltda/factorialservice/factorial/FactorialService.java
+++ factorial-service/src/main/java/com/calcltda/factorialservice/factorial/FactorialService.java
@@ -2,6 +2,7 @@ package com.calcltda.factorialservice.factorial;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
@@ -11,15 +12,18 @@ public class FactorialService {
     private final Logger log = LoggerFactory.getLogger(FactorialService.class);
 
     private final FactorialRepository factorialRepository;
+    private final RabbitTemplate rabbitTemplate;
 
-    public FactorialService(FactorialRepository factorialRepository) {
+    public FactorialService(FactorialRepository factorialRepository, RabbitTemplate rabbitTemplate) {
         this.factorialRepository = factorialRepository;
+        this.rabbitTemplate = rabbitTemplate;
     }
 
     public Mono<FactorialDTO> createFactorialRequest(FactorialDTO factorialDTO){
         return factorialRepository
                 .save(Factorial.fromDTO(factorialDTO))
                 .map(FactorialDTO::fromFactorial)
+                .doOnNext(f -> rabbitTemplate.convertAndSend("amq.direct", "factorial", f))
                 .doOnSuccess(factorial -> log.info("Created: {}", factorial))
                 .doOnError(ex -> log.error("Error during factorial creation: {}", ex.getMessage()));
     }
