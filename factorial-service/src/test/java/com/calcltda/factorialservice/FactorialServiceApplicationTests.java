3b902bd974d58155f2f7d452196a360d91965e11
Jorge Henrique <jorgehasd@gmail.com>
2023-12-26 08:15:38 -0300

    feat: add factorial service project

---
+++ factorial-service/src/test/java/com/calcltda/factorialservice/FactorialServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calcltda.factorialservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactorialServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
