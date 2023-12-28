6119c9bd66d526000206a118abac98108627e11a
Jorge Henrique <jorgehasd@gmail.com>
2023-12-28 11:50:06 -0300

    feat: random requests from data csv

--- k6/script.js
+++ k6/script.js
@@ -1,21 +1,23 @@
 import http from "k6/http";
-import { SharedArray } from 'k6/data';
 import { check } from 'k6';
+import { SharedArray } from 'k6/data';
+import papaparse from 'https://jslib.k6.io/papaparse/5.1.1/index.js';
 
 export const options = {
-  vus: 10,
-  duration: '20s'
+  vus: 1,
+  duration: '1s'
 };
 
-//const factorials = new SharedArray('factorials', function () {
-//  return JSON.parse(open('./data.json')).factorials;
-//});
+const csvData = new SharedArray('factorials', function () {
+  return papaparse.parse(open('./data.csv'), { header: true }).data;
+});
 
 export default function () {
   const url = 'http://factorial-service:8085/factorial';
 
+  const random = csvData[Math.floor(Math.random() * csvData.length)];
   const payload = JSON.stringify({
-    number: 30,
+    number: random.number,
   });
 
   const params = {
